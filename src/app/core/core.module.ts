a8f5c2ff7d0fb04272f0bfca451359f4378bce5a
Esteban Manrupe <esteban_manrupe@hotmail.com>
2023-02-09 08:18:49 -0300

    add sections in sidebar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
 import { HomeComponent } from '../home/components/home.component';
 import { SearchComponent } from '../search/components/search.component';
+import { MyMusicComponent } from '../my-music/components/my-music.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchComponent },
   // { path: 'profile', component: ProfileComponent },
+  { path: 'myMusic', component: MyMusicComponent },
 ];
 @NgModule({
   declarations: [SidebarComponent],
