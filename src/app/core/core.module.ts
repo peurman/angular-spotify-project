47b3acaaec85bf359f8f89376e9790180eb8f41f
Esteban Manrupe <esteban_manrupe@hotmail.com>
2023-02-10 19:39:24 -0300

    move forward  with Album page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,6 +6,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomeComponent } from '../home/components/home.component';
 import { SearchComponent } from '../search/components/search.component';
 import { MyMusicComponent } from '../my-music/components/my-music.component';
+import { LoadingComponent } from './components/loading/loading.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -14,8 +15,8 @@ const routes: Routes = [
   { path: 'myMusic', component: MyMusicComponent },
 ];
 @NgModule({
-  declarations: [SidebarComponent],
-  exports: [SidebarComponent, RouterModule],
+  declarations: [SidebarComponent, LoadingComponent],
+  exports: [SidebarComponent, RouterModule, LoadingComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class CoreModule {}
--- src/app/my-music/my-music.module.ts
+++ src/app/my-music/my-music.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyMusicRoutingModule } from './my-music-routing.module';
 import { MyMusicComponent } from './components/my-music.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [MyMusicComponent],
-  imports: [CommonModule, MyMusicRoutingModule],
+  imports: [CommonModule, MyMusicRoutingModule, CoreModule],
 })
 export class MyMusicModule {}
