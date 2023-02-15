f4cbbf73f9585537f0b24be699ea80d3f04f6e3d
Esteban Manrupe <esteban_manrupe@hotmail.com>
2023-02-15 10:07:22 -0300

    change current Url for deploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,4 +3,7 @@ export const environment = {
     clientId: '63f6c3628183493388e27b2bc9ea636f',
     clientSecret: 'fb710436d9be4066965a591268917800',
   },
+  production: false,
+  apiUrl: 'http://localhost:4200',
+  isLocal: true,
 };
