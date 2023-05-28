9efaccbc74ad4c36f119a73e7e6e85fc45e20148
Esteban Manrupe <esteban_manrupe@hotmail.com>
2023-05-28 11:02:00 -0300

    update environments

--- src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  credentials: {
-    clientId: '63f6c3628183493388e27b2bc9ea636f',
-    clientSecret: 'fb710436d9be4066965a591268917800',
-  },
-  production: false,
-  apiUrl: 'http://localhost:4200',
-  isLocal: true,
-};
